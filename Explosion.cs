8a57adc13f83d916b8bcf7842531a4baee107344
Daniel Haacke <dhaacke@umich.edu>
2020-03-27 14:54:06 -0400

    Initial commit

---
+++ Explosion.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalWindows {
+    public class Explosion : IDrawable {
+        public void Draw(CanvasAnimatedControl canvas) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalWindows {
+    public interface IDrawable {
+        void Draw(CanvasAnimatedControl canvas);
+    }
+}
